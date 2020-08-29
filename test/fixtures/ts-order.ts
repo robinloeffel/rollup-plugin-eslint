b5c27408dc47567c186f42f9dd662c5d6e70c268
Robin Löffel <hi@robinloeffel.ch>
2020-08-29 12:24:26 +0200

    add test a test scenario for typescript

---
+++ test/fixtures/ts-order.ts
@@ -0,0 +1,5 @@
+const pluginName = '@rbnlffl/rollup-plugin-eslint';
+const logItOutYo = (thangToLog: string): void => {
+  console.log(thangToLog);
+};
+logItOutYo(pluginName);
--- test/test.js
+++ test/test.js
@@ -2,30 +2,22 @@ const fs = require('fs');
 const test = require('ava');
 const eslint = require('../src');
 const { rollup } = require('rollup');
+const typescript = require('@rollup/plugin-typescript');
+const { terser } = require('rollup-plugin-terser');
 
-test.after(() => {
-  // revert `will-fix.js` to contain `var` instead of `const`
-  const path = 'test/fixtures/will-fix.js';
-  const fixed = fs.readFileSync(path).toString();
-  const reverted = fixed.replace(/const/g, 'var');
-  fs.writeFileSync(path, reverted);
-});
-
-test('rollup runs with the plugin', async t => {
+test('runs with the plugin', async t => {
   const bundle = await rollup({
     input: 'test/fixtures/all-good.js',
     plugins: [
       eslint()
     ]
   });
-  const { output } = await bundle.generate({
-    file: 'test/fixtures/all-good.js'
-  });
+  const { output } = await bundle.generate({ file: '' });
 
   t.true(output[0].code.length > 0);
 });
 
-test('rollup runs with the plugin and a configuration', async t => {
+test('runs with the plugin and a configuration', async t => {
   const bundle = await rollup({
     input: 'test/fixtures/all-good.js',
     plugins: [
@@ -35,39 +27,59 @@ test('rollup runs with the plugin and a configuration', async t => {
       })
     ]
   });
-  const { output } = await bundle.generate({
-    file: 'test/fixtures/all-good.js'
-  });
+  const { output } = await bundle.generate({ file: '' });
 
   t.true(output[0].code.length > 0);
 });
 
-test('eslint autofix works', async t => {
+test('autofix works', async t => {
   const bundle = await rollup({
     input: 'test/fixtures/will-fix.js',
     plugins: [
-      eslint({
-        fix: true
-      })
+      eslint({ fix: true })
     ]
   });
-  const { output } = await bundle.generate({
-    file: 'test/fixtures/will-fix.js'
-  });
+  const { output } = await bundle.generate({ file: '' });
+
+  // revert file contents
+  const fixed = fs.readFileSync('test/fixtures/will-fix.js').toString();
+  const reverted = fixed.replace(/const/g, 'var');
+  fs.writeFileSync('test/fixtures/will-fix.js', reverted);
 
   t.true(output[0].code.includes('const func'));
 });
 
-test('eslint ignores node_modules', async t => {
+test('ignores node_modules', async t => {
   const bundle = await rollup({
     input: 'test/fixtures/no-node-modules.js',
     plugins: [
       eslint()
     ]
   });
-  const { output } = await bundle.generate({
-    file: 'test/fixtures/no-node-modules.js'
-  });
+  const { output } = await bundle.generate({ file: '' });
 
   t.true(output[0].code.length > 0);
+});
+
+test('runs with typescript', async t => {
+  const bundle = await rollup({
+    input: 'test/fixtures/ts-order.ts',
+    plugins: [
+      eslint({
+        overrideConfig: {
+          parser: '@typescript-eslint/parser',
+          plugins: [ '@typescript-eslint' ],
+          extends: [
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended'
+          ]
+        }
+      }),
+      typescript(),
+      terser()
+    ]
+  });
+  const { output } = await bundle.generate({ file: '' });
+
+  t.true(output[0].code.includes('var'));
 });
